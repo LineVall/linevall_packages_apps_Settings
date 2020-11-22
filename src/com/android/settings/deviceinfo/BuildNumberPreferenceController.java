489f3616b0b140777e62d9433ff468e27f6c9c71
Jyotiraditya Panda <jyotiraditya@aospa.co>
2020-11-22 12:00:03 +0000

    Show only Build ID in about phone
    Change-Id: I5e91efc7b82717e8f0d0abb113ab910ed46d8903

--- src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
+++ src/com/android/settings/deviceinfo/BuildNumberPreferenceController.java
@@ -80,7 +80,7 @@ public class BuildNumberPreferenceController extends BasePreferenceController im
 
     @Override
     public CharSequence getSummary() {
-        return BidiFormatter.getInstance().unicodeWrap(Build.DISPLAY);
+        return BidiFormatter.getInstance().unicodeWrap(Build.ID);
     }
 
     @Override
